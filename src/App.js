c15e05e16c21ea257a0942b8bfc6f00b19a13514
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-03-02 16:43:13 -0500

    add Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Pokedex from './components/Pokedex/Pokedex';
+import Header from './components/Header/Header';
 
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Pokedex />
 
     </div>
