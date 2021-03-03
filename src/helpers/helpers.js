553d7ef226fb9cf37e235fb94eb86ccb0be64077
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-03-03 00:21:10 -0500

    export each function individually

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,29 +1,31 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
+
+export const BASE_URL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
 const LENGTH = BASE_URL.length;
 
-function isEmpty(arr) {
+export function isEmpty(arr) {
   return arr.length === 0;
 }
 
-function createIDFromUrl(url) {
+export function createIDFromUrl(url) {
   return url.substring(LENGTH + 1, url.length - 1)
 }
 
-function findById(name, allPokemon) {
+export function findById(name, allPokemon) {
   return allPokemon.find(p => p.name === name)
 }
 
-function capitalize(str) {
+export function capitalize(str) {
   return str[0].toUpperCase() + str.substring(1);
 }
 
-function convertWeight(weight) {
+export function convertWeight(weight) {
   // convert from hectograms to lbs
   return (weight / 4.5359237).toFixed(1);
 
 }
 
-function shortenStatName(stat) {
+export function shortenStatName(stat) {
   if (stat.length < 3) return stat.toUpperCase();
 
   if (stat.includes('-')) {
@@ -36,19 +38,8 @@ function shortenStatName(stat) {
   return stat.substring(0, 3).toUpperCase();
 }
 
-function convertHeight(height) {
+export function convertHeight(height) {
   // decimeter to ft
-
   return (height / 3.048).toFixed(1);
 }
 
-export {
-  BASE_URL,
-  isEmpty,
-  createIDFromUrl,
-  findById,
-  capitalize,
-  shortenStatName,
-  convertWeight,
-  convertHeight
-}
