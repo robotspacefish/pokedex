715016178a4531d035c2369b6e23ab38654a939f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-03-09 16:52:09 -0500

    WIP back to top button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Pokedex from './components/Pokedex/Pokedex';
 import Header from './components/Header/Header';
+import BackToTopButton from './components/BackToTopButton/BackToTopButton';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">s
       <Header />
       <Pokedex />
-
+      {/* <BackToTopButton /> */}
     </div>
   );
 }
